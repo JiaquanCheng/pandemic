392cf4d008aaa40ccd5fe3210970ff373094d03b
oldch <oldch@LAPTOP-EO3MD4NB.localdomain>
2021-05-02 19:54:49 +0200

    hey

---
+++ alan/src/alan/andrew.java
@@ -0,0 +1,14 @@
+package alan;
+
+public class andrew {
+
+	public andrew() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
